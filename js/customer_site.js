860906389b8e1d622336b600c81e42ce6e9e8b62
Jack4223 <jack4223@gmail.com>
2025-09-25 17:10:52 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,16 +1,4 @@
 const CUSTOMER_SITES = {
-    ix: {
-        api: 'https://cj.lziapi.com/api.php/provide/vod',
-        name: '影視工廠',
-    }
-    bf: {
-        api: 'https://bfzyapi.com/api.php/provide/vod',
-        name: '暴風資源',
-    }
-    ff: {
-        api: 'http://api.ffzyapi.com/api.php/provide/vod/from/feifan/at/xml',
-        name: '非凡影視',
-    }
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',

0c8dab9feb0ab7f5ce0ff7bb5bd57e51a55275bf
Jack4223 <jack4223@gmail.com>
2025-09-25 17:07:06 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,5 +1,17 @@
 const CUSTOMER_SITES = {
     qiqi: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod',
+        name: '影視工廠',
+    }
+    qiqi: {
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴風資源',
+    }
+    qiqi: {
+        api: 'http://api.ffzyapi.com/api.php/provide/vod/from/feifan/at/xml',
+        name: '非凡影視',
+    }
+    qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }

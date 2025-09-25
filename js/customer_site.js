49a38902c0821de1df2e24064d6bc809580d46a5
Jack4223 <jack4223@gmail.com>
2025-09-25 18:29:48 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,27 @@
 const CUSTOMER_SITES = {
     bf: {
         api: 'https://bfzyapi.com/api.php/provide/vod',
-        name: '暴风资源',
+        name: '暴風資源',
+    },
+    ry: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意資源',
+    },
+    ty: {
+        api: 'https://tyyszy.com/api.php/provide/vod',
+        name: '天涯資源',
+    },
+    360: {
+        api: 'https://360zy.com/api.php/provide/vod',
+        name: '360資源',
+    },
+    fy: {
+        api: 'https://fengyunapi.com/api.php/provide/vod',
+        name: '風雲資源',
+    },
+    fy: {
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        name: '茅台資源',
     }
 };
 
